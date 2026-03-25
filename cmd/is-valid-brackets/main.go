dfa9ba683b285a16630b8073cfe5abbdd6f69ec0
Eli Itah <elijahbit@gmail.com>
2026-03-25 12:10:26 -0500

    With list data model

--- cmd/is-valid-brackets/main.go
+++ cmd/is-valid-brackets/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
-	"slices"
 )
 
 func main() {
@@ -13,38 +13,60 @@ func main() {
 }
 
 func isValid(s string) bool {
-	stack := []rune{}
-	openings := []rune{'{', '(', '['}
-	closings := []rune{'}', ')', ']'}
+	stack := list.New()
+	closeToOpen := map[rune]rune{'}': '{', ']': '[', ')': '('}
 
-	for _, char := range s {
-		if slices.Contains(openings, char) {
-			stack = append(stack, char)
-		}
-		if slices.Contains(closings, char) {
-			if len(stack) == 0 {
+	for _, c := range s {
+		if open, exists := closeToOpen[c]; exists {
+			if stack.Len() != 0 {
+				top := stack.Remove(stack.Front())
+				if top.(rune) != open {
+					return false
+				}
+			} else {
 				return false
 			}
-			lenEnd := len(stack) - 1
-			if char == '}' && stack[lenEnd] == '{' ||
-				char == ')' && stack[lenEnd] == '(' ||
-				char == ']' && stack[lenEnd] == '[' {
-				stack = stack[:lenEnd]
-				// fmt.Println("delete", stack)
-
-				continue
-			}
-			return false
+		} else {
+			stack.PushFront(c)
 		}
-		// fmt.Println("stack", stack)
 	}
 
-	if len(stack) == 0 {
-		return true
-	}
-	// has unclosed items
-	return false
+	return stack.Len() == 0
 }
+
+// func isValid(s string) bool {
+// 	stack := []rune{}
+// 	openings := []rune{'{', '(', '['}
+// 	closings := []rune{'}', ')', ']'}
+
+// 	for _, char := range s {
+// 		if slices.Contains(openings, char) {
+// 			stack = append(stack, char)
+// 		}
+// 		if slices.Contains(closings, char) {
+// 			if len(stack) == 0 {
+// 				return false
+// 			}
+// 			lenEnd := len(stack) - 1
+// 			if char == '}' && stack[lenEnd] == '{' ||
+// 				char == ')' && stack[lenEnd] == '(' ||
+// 				char == ']' && stack[lenEnd] == '[' {
+// 				stack = stack[:lenEnd]
+// 				// fmt.Println("delete", stack)
+
+// 				continue
+// 			}
+// 			return false
+// 		}
+// 		// fmt.Println("stack", stack)
+// 	}
+
+// 	if len(stack) == 0 {
+// 		return true
+// 	}
+// 	// has unclosed items
+// 	return false
+// }
 
 // this approach doesnt work bc it assumes the brackets are only ever nested, we dont handle the case of sibling bracket
 // options
